18a8f34e946bb32e2ed7b9ee369166a2c63815ec
mkanderson2024 <kalani2014.ma@gmail.com>
2025-06-16 17:55:24 -0600

    saving work

---
+++ scripts/utilities.js
@@ -0,0 +1,10 @@
+//-------Copyright Date Script-------
+const date = new Date;
+const year = document.getElementById("copyright-year");
+
+year.innerHTML += date.getFullYear();
+
+//Last Modified Script
+const modified = document.getElementById("lastModified");
+let lastModified = new Date(document.lastModified);
+modified.innerHTML += lastModified;
